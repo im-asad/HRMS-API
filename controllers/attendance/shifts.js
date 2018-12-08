883aec0c9f095ba04bb5d7b7b3c3e3a9e5554c64
Daniyal <mdaniyal.kh@gmail.com>
2018-12-08 18:46:13 +0500

    fix

--- controllers/attendance/shifts.js
+++ controllers/attendance/shifts.js
@@ -1,7 +1,4 @@
 
-const Shift = models.Shift
-const Employee = models.Employee
-const Attendance = models.Attendance
 const moment = require('moment-business-days')
 
 
@@ -31,7 +28,7 @@ module.exports = (sequelize) => {
 				})
 			}
 		})
-	}},
+	},
 
 	generateMonthlyShiftForEmployee: async (shift_id, machineCode)=>{
 		var startDate = moment(new Date()).subtract(1, "days");
@@ -67,39 +64,5 @@ module.exports = (sequelize) => {
 		})
 	}
 
-})
-
-
-
-
-
-
-
-// function generateMonthShifts(month) {
-// 	// For each employeeShift (get shift and employee)
-// 	// For each day
-// 	moment()
-// 		.monthBusinessDays()
-// 		.forEach(date => {
-// 			// TO DO: Create entry for this day.
-// 		})
-// }
-
-function setEmployeeShift(employee_code, shift_id, date) {
-	Attendance.create({
-		machineCode: employee_code,
-		shift_id: shift_id,
-		date: date,
-		status: 'N/A',
-		leave: false,
-	})
 }
-
-function getAttendanceDetails(employee, startDate, endDate) {
-	// For each attendance entry of that employee
-	// return details
-}
-
-function recordTime(type, time) {
-	// if type is outgoing, then update status
 }

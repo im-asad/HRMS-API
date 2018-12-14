99b5a66bb95e9fdd981e9a9f4e1fd47e020e53f0
Asad <asad.awan,se@gmail.com>
2018-12-14 18:01:52 +0500

    user privileges

--- models/security/index.js
+++ models/security/index.js
@@ -1,3 +1,4 @@
 module.exports = sequelize => ({
     Role: sequelize.import('./role.js'),
+    Permission: sequelize.import('./permission.js'),
 });
---
+++ models/security/permission.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+
+module.exports = (sequelize) => {
+    const Permission = sequelize.define("permission", {
+        permission_id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        permissions: {
+            type: Sequelize.TEXT,
+        }
+    })
+
+    Permission.associate = (models) => {
+        Permission.belongsTo(models.Role, {foreignKey: 'roleId', sourceKey: "role_id"});
+    }
+
+    return Permission;
+}

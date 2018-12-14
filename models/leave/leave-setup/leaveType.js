668b3a0578de8a5d6cc9cd070a080341532ce3fb
Hussain Nawaz Lalee <38792700+Lalee10@users.noreply.github.com>
2018-12-14 17:28:47 +0500

    Super Master Fixed

--- models/leave/leave-setup/leaveType.js
+++ models/leave/leave-setup/leaveType.js
@@ -1,16 +1,16 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize')
 
-module.exports = (sequelize) => {
-    const LeaveType = sequelize.define("leaveType", {
-        leaveType_id: {
-            type:Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        quantity: {
-            type: Sequelize.INTEGER
-        }
-    })
+module.exports = sequelize => {
+	const LeaveType = sequelize.define('leaveType', {
+		leaveType_id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		quantity: {
+			type: Sequelize.INTEGER,
+		},
+	})
 
-    return LeaveSetup;
+	return LeaveType
 }

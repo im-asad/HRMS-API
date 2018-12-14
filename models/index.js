04303693029f1d737090ae9089b5c9c5e3667f89
Hussain Nawaz Lalee <38792700+Lalee10@users.noreply.github.com>
2018-12-14 18:36:00 +0500

    Role Model Added

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ module.exports = sequelize => {
 	var setupModels = require('./setup/')(sequelize)
 	/* var leaveModels = require('./leave/')(sequelize); */
 	var attendanceModels = require('./attendance/')(sequelize)
-	var models = Object.assign({}, setupModels /* , leaveModels */, attendanceModels)
+	var securityModels = require('./security/')(sequelize)
+	var models = Object.assign({}, setupModels /* , leaveModels */, attendanceModels, securityModels)
 
 	console.log(models)
 
